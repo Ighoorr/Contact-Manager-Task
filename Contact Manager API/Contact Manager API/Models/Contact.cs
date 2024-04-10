e03d3ac90d89d1e0eac4db5aa5a391a0ba44bed9
Ighoorr <i.bereshchak@gmail.com>
2024-04-10 22:57:16 +0300

    models and controlers

---
+++ Contact Manager API/Contact Manager API/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Contact_Manager_API.Models
+{
+    public partial class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool? Married { get; set; }
+        public string Phone { get; set; }
+        public decimal? Salary { get; set; }
+    }
+}
---
+++ Contact Manager API/Contact Manager API/Models/ContactManagerContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Contact_Manager_API.Models
+{
+    public partial class ContactManagerContext : DbContext
+    {
+        public ContactManagerContext()
+        {
+        }
+
+        public ContactManagerContext(DbContextOptions<ContactManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-HTNFDBR;Database=ContactManager;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Ukrainian_CI_AS");
+
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.ToTable("Contact");
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Phone).HasMaxLength(100);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(5, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
